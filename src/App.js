d3cf54d3484c6757b71a744345a0e60db5a6d166
gtretow <guilherme.tretow@gmail.com>
2021-01-13 02:21:08 -0300

    redux properties tests now pass

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import { connect } from "react-redux";
 
 import GuessedWords from "./GuessedWords";
 import Congrats from "./Congrats";
+import Input from "./Input";
+import { getSecretWord } from "./actions";
 
-function App() {
-  return (
-    <div className="container">
-      {/* hard code pois não temos a parte do Redux/hooks ainda */}
-      <h1>The Jotto Game!</h1>
-      <Congrats success={true} />
-      <GuessedWords guessedWords={[{guessedWord: 'train', letterMatchCount: 3}]} />
+export class App extends Component {
+  /**
+   * @method componentDidMount
+   * @returns {undefined}
+   */
+  componentDidMount() {
+    // get the secret word
+    this.props.getSecretWord();
+  }
 
-    </div>
-  );
+  render() {
+    return (
+      <div className="container">
+        <h1>Jotto</h1>
+        <Congrats success={this.props.success} />
+        <Input />
+        <GuessedWords guessedWords={this.props.guessedWords} />
+      </div>
+    );
+  }
 }
+const mapStateToProps = (state) => {
+  const { success, guessedWords, secretWord } = state;
 
-export default App;
+  return { success, guessedWords, secretWord };
+};
+export default connect(mapStateToProps, { getSecretWord })(App);
